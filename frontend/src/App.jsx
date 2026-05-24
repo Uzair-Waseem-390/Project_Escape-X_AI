86047b40928d64ff8d8c03b3a93f1ed43f5ccc58
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-05-25 01:24:43 +0500

    install the tailwind and ready for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import './App.css'
-
-function App() {
-
-
+export default function App() {
   return (
-    <>
-    </>
+    <h1 className="text-4xl font-bold text-blue-500">
+      Tailwind is working
+    </h1>
   )
 }
-
-export default App
